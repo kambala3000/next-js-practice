7d7c42755dabfdf08d02c549b9068e398487df38
kambala3000 <kambala02@gmail.com>
2019-04-22 11:45:18 +0300

    use Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import Link from "next/link"
+
 const Index = () => (
   <div>
+    <Link href="/about">
+      <button>Go to About Page</button>
+    </Link>
+
     <p>Hello Next.js</p>
   </div>
 )
